580b80b593245b321c65bd1c53b23ecb8e7878cd
raajok <jokinen.rasmus@gmail.com>
2026-04-03 21:01:30 +0300

    Exercise 20

--- todo-app/todo-frontend/test/Todo.test.jsx
+++ todo-app/todo-frontend/test/Todo.test.jsx
@@ -3,12 +3,14 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Todo from '../src/Todos/Todo'
 
+// For some reason the test fails on my environment because of the @rolldown library,
+// but works when building the docker image
 test('Todo shows text', () => {
   const todo = {
     text: "text shown",
     done: true
   }
 
-  render(<Todo todo={todo} />)
+  render(<Todo todo={todo} deleteTodo={() => undefined} completeTodo={() => undefined} />)
   expect(screen.getByText(todo.text)).toBeInTheDocument()
 })
